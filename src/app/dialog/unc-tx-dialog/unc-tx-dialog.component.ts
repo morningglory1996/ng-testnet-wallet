8f2d5a0658ef1f81c4e9dbaa7e835f4c30a93f0c
morningglory1996 <acer080428@outlook.jp>
2020-07-10 14:58:53 +0900

    fix scss

--- src/app/dialog/unc-tx-dialog/unc-tx-dialog.component.ts
+++ src/app/dialog/unc-tx-dialog/unc-tx-dialog.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Inject, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 
@@ -24,10 +27,17 @@ export class UncTxDialogComponent implements OnInit {
   displayedColumns: string[] = ['date', 'txId'];
   dataSource = new MatTableDataSource<Transaction>(this.data);
 
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  .pipe(
+    map(result => result.matches),
+    shareReplay()
+  );
+
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data : Transaction[],
+    private breakpointObserver: BreakpointObserver,
     public dialogRef: MatDialogRef<UncTxDialogComponent>
   ) { }
 
